19ded22749882012081c0ec9b47d4a7e6b9c128f
Yixuan Qiu <yixuanq@gmail.com>
2015-02-28 08:59:50 -0500

    update client code

--- test.cpp
+++ test.cpp
@@ -12,13 +12,14 @@ int main()
     int m = 6;
 
     Eigen::SelfAdjointEigenSolver<MatrixXd> eig(A);
-    std::cout << "true eigenvalues = \n" << eig.eigenvalues() << std::endl;
+    std::cout << "true eigenvalues = \n" << eig.eigenvalues() << "\n\n";
 
-    SymEigsSolver eigs(A, k, m);
+    DenseMatOp<double> op(A);
+    SymEigsSolver<double> eigs(&op, k, m);
     int niter = eigs.compute();
 
+    std::cout << "computed eigenvalues = \n" << eigs.eigenvalues() << "\n\n";
     std::cout << "niter = " << niter << std::endl;
-    std::cout << "computed eigenvalues = \n" << eigs.eigenvalues() << std::endl;
 
     return 0;
 }
