b7c6b888b2e979bea26521875b6ee69368d8bed9
Yixuan Qiu <yixuan.qiu@cos.name>
2015-02-28 15:02:02 -0500

    comments and documentation

--- include/SymEigsSolver.h
+++ include/SymEigsSolver.h
@@ -17,25 +17,27 @@ private:
     typedef Eigen::Array<Scalar, Eigen::Dynamic, 1> Array;
     typedef Eigen::Array<bool, Eigen::Dynamic, 1> BoolArray;
     typedef Eigen::Map<const Matrix> MapMat;
+    typedef Eigen::Map<const Vector> MapVec;
     typedef Eigen::SelfAdjointEigenSolver<Matrix> EigenSolver;
     typedef Eigen::HouseholderQR<Matrix> QRdecomp;
     typedef Eigen::HouseholderSequence<Matrix, Vector> QRQ;
     typedef std::pair<Scalar, int> SortPair;
 
-    MatOp<Scalar> *op;
-    int dim_n;           // dimension of A
+    MatOp<Scalar> *op;   // object to conduct matrix operation,
+                         // e.g. matrix-vector product
+    int dim_n;           // dimension of matrix A
     int nev;             // number of eigenvalues requested
-    int ncv;             // "m"
-    int fac_step;        // how many steps in the Arnoldi factorization
-    Matrix fac_V;
-    Matrix fac_H;
-    Vector fac_f;
+    int ncv;             // number of ritz values
 
-    Vector ritz_val;
-    Matrix ritz_vec;
-    BoolArray ritz_conv;
+    Matrix fac_V;        // V matrix in the Arnoldi factorization
+    Matrix fac_H;        // H matrix in the Arnoldi factorization
+    Vector fac_f;        // residual in the Arnoldi factorization
 
-    // Arnoldi factorization
+    Vector ritz_val;     // ritz values
+    Matrix ritz_vec;     // ritz vectors
+    BoolArray ritz_conv; // indicator of the convergence of ritz values
+
+    // Arnoldi factorization starting from step-k
     void factorize_from(int from_k, int to_m, const Vector &fk)
     {
         if(to_m <= from_k) return;
@@ -58,50 +60,18 @@ private:
             fac_f = w - fac_V.leftCols(i + 1) * h;
             fac_H.block(0, i, i + 1, 1) = h;
         }
-
-        fac_step = to_m;
     }
 
+    // The step-m Arnoldi factorization
     void factorize()
     {
         factorize_from(1, ncv, fac_f);
         retrieve_ritzpair();
     }
 
-    static bool compare_eigenvalue(SortPair p1, SortPair p2)
-    {
-        return p1.first > p2.first;
-    }
-
-    void retrieve_ritzpair()
-    {
-        EigenSolver eig(fac_H);
-        Vector evals = eig.eigenvalues();
-        Matrix evecs = eig.eigenvectors();
-
-        std::vector<SortPair> pairs(ncv);
-        for(int i = 0; i < ncv; i++)
-        {
-            pairs[i].first = evals[i];
-            pairs[i].second = i;
-        }
-
-        std::sort(pairs.begin(), pairs.end(), compare_eigenvalue);
-
-        for(int i = 0; i < ncv; i++)
-        {
-            ritz_val[i] = pairs[i].first;
-        }
-        for(int i = 0; i < nev; i++)
-        {
-            ritz_vec.col(i) = evecs.col(pairs[i].second);
-        }
-    }
-
+    // Implicitly restarted Arnoldi factorization
     void restart(int k)
     {
-        if(fac_step != ncv)
-            return;
         if(k >= ncv)
             return;
 
@@ -119,6 +89,7 @@ private:
             // H -> Q'HQ
             fac_H.applyOnTheRight(Q);
             fac_H.applyOnTheLeft(Q.adjoint());
+            // em -> Q'em
             em.applyOnTheLeft(Q.adjoint());
         }
 
@@ -129,7 +100,10 @@ private:
 
     bool converged(Scalar tol)
     {
+        // prec = epsilon^(2/3)
+        // epsilon is the machine precision, e.g. ~= 1e-16 for the "double" type
         Scalar prec = std::pow(std::numeric_limits<Scalar>::epsilon(), Scalar(2.0 / 3));
+        // bound = tol * max(prec, abs(theta)), theta for ritz value
         Array bound = tol * ritz_val.head(nev).array().abs().max(prec);
         Array resid =  ritz_vec.bottomRows(1).transpose().array().abs() * fac_f.norm();
         ritz_conv = (resid < bound);
@@ -137,23 +111,34 @@ private:
         return ritz_conv.all();
     }
 
-    void init()
+    static bool compare_eigenvalue(SortPair p1, SortPair p2)
     {
-        fac_V.setZero();
-        fac_H.setZero();
-        fac_f.setZero();
-        ritz_val.setZero();
-        ritz_vec.setZero();
-        ritz_conv.setZero();
+        return p1.first > p2.first;
+    }
 
-        Vector v = Vector::Random(dim_n);
-        v.normalize();
-        Vector w(dim_n);
-        op->prod(v.data(), w.data());
+    // Retrieve and sort ritz values and ritz vectors
+    void retrieve_ritzpair()
+    {
+        EigenSolver eig(fac_H);
+        Vector evals = eig.eigenvalues();
+        Matrix evecs = eig.eigenvectors();
 
-        fac_H(0, 0) = v.dot(w);
-        fac_f = w - v * fac_H(0, 0);
-        fac_V.col(0) = v;
+        std::vector<SortPair> pairs(ncv);
+        for(int i = 0; i < ncv; i++)
+        {
+            pairs[i].first = evals[i];
+            pairs[i].second = i;
+        }
+        std::sort(pairs.begin(), pairs.end(), compare_eigenvalue);
+
+        for(int i = 0; i < ncv; i++)
+        {
+            ritz_val[i] = pairs[i].first;
+        }
+        for(int i = 0; i < nev; i++)
+        {
+            ritz_vec.col(i) = evecs.col(pairs[i].second);
+        }
     }
 
 public:
@@ -162,17 +147,42 @@ public:
         dim_n(op->rows()),
         nev(nev_),
         ncv(ncv_),
-        fac_step(1),
         fac_V(dim_n, ncv),
         fac_H(ncv, ncv),
         fac_f(dim_n),
         ritz_val(ncv),
         ritz_vec(ncv, nev),
         ritz_conv(nev)
+    {}
+
+    // Initialization and clean-up
+    void init(Scalar *init_coef)
     {
-        init();
+        fac_V.setZero();
+        fac_H.setZero();
+        fac_f.setZero();
+        ritz_val.setZero();
+        ritz_vec.setZero();
+        ritz_conv.setZero();
+
+        Vector v(dim_n);
+        op->prod(init_coef, v.data());
+        v.normalize();
+        Vector w(dim_n);
+        op->prod(v.data(), w.data());
+
+        fac_H(0, 0) = v.dot(w);
+        fac_f = w - v * fac_H(0, 0);
+        fac_V.col(0) = v;
+    }
+    // Initialization with random initial coefficients
+    void init()
+    {
+        Vector init_coef = Vector::Random(dim_n);
+        init(init_coef.data());
     }
 
+    // Compute eigenvalues and return the number of iteration
     int compute(int maxit = 1000, Scalar tol = 1e-10)
     {
         factorize();
@@ -189,6 +199,7 @@ public:
         return i + 1;
     }
 
+    // Return converged eigenvalues
     Vector eigenvalues()
     {
         int nconv = ritz_conv.cast<int>().sum();
@@ -210,6 +221,7 @@ public:
         return res;
     }
 
+    // Return converged eigenvectors
     Matrix eigenvectors()
     {
         int nconv = ritz_conv.cast<int>().sum();
