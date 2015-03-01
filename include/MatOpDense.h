9566093b71f1a9b60a2aff366513ef4dd0fa1248
Yixuan Qiu <yixuan.qiu@cos.name>
2015-03-01 16:18:11 -0500

    add real shift solver

--- include/MatOpDense.h
+++ include/MatOpDense.h
@@ -12,13 +12,30 @@ private:
     typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> Matrix;
     typedef Eigen::Map<const Matrix> MapMat;
     typedef Eigen::Map< Eigen::Matrix<Scalar, Eigen::Dynamic, 1> > MapVec;
+    typedef Eigen::PartialPivLU<Matrix> RealSolver;
+    typedef Eigen::PartialPivLU<Eigen::MatrixXcd> ComplexSolver;
 
     MapMat mat;
     MapVec vec_x;
     MapVec vec_y;
 
+    RealSolver rsolver;
+    ComplexSolver csolver;
+
+    // shift solve for real sigma
+    virtual void real_shift_solve(Scalar *x_in, Scalar *y_out)
+    {
+        new (&vec_x) MapVec(x_in, mat.cols());
+        new (&vec_y) MapVec(y_out, mat.rows());
+
+        vec_y.noalias() = rsolver.solve(vec_x);
+    }
+
     // shift solve for complex sigma
-    virtual void complex_shift_solve(Scalar *x_in, Scalar *y_out) {}
+    virtual void complex_shift_solve(Scalar *x_in, Scalar *y_out)
+    {
+
+    }
 public:
     MatOpDense(const Matrix &mat_) :
         MatOp<Scalar>(mat_.rows(), mat_.cols()),
@@ -34,8 +51,8 @@ public:
     // y_out = A * x_in
     virtual void prod(Scalar *x_in, Scalar *y_out)
     {
-        new (&vec_x) MapVec(x_in, MatOp<Scalar>::cols());
-        new (&vec_y) MapVec(y_out, MatOp<Scalar>::rows());
+        new (&vec_x) MapVec(x_in, mat.cols());
+        new (&vec_y) MapVec(y_out, mat.rows());
 
         vec_y.noalias() = mat * vec_x;
     }
@@ -43,14 +60,20 @@ public:
     // y_out = A' * x_in
     virtual void trans_prod(Scalar *x_in, Scalar *y_out)
     {
-        new (&vec_x) MapVec(x_in, MatOp<Scalar>::rows());
-        new (&vec_y) MapVec(y_out, MatOp<Scalar>::cols());
+        new (&vec_x) MapVec(x_in, mat.rows());
+        new (&vec_y) MapVec(y_out, mat.cols());
 
         vec_y.noalias() = mat.transpose() * vec_x;
     }
 
-    // y_out = inv(A - sigma * I) * x_in
-    virtual void shift_solve(Scalar *x_in, Scalar *y_out)
+    // setting real shift
+    virtual void set_real_shift(Scalar sigma)
+    {
+        rsolver.compute(mat - sigma * Matrix::Identity(mat.rows(), mat.cols()));
+    }
+
+    // setting complex shift
+    virtual void set_complex_shift(Scalar sigmar, Scalar sigmai)
     {
 
     }
