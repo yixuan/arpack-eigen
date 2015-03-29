95f26c30d802ed209439e6567449230b9c7a7db7
Yixuan Qiu <yixuan.qiu@cos.name>
2015-03-29 11:06:48 -0400

    test program

---
+++ test/SymEigs.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <SymEigsSolver.h>
+#include <GenEigsSolver.h>
+#include <MatOpDense.h>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+template <int SelectionRule>
+void test(const MatrixXd &A, int k, int m)
+{
+    MatrixXd mat;
+    if(SelectionRule == BOTH_ENDS)
+    {
+        mat = A.adjoint() + A;
+    } else {
+        mat = A.adjoint() * A;
+    }
+
+    Eigen::SelfAdjointEigenSolver<MatrixXd> eig(mat);
+    std::cout << "true eigenvalues = \n" << eig.eigenvalues().transpose() << "\n";
+
+    MatOpDense<double> op(mat);
+    SymEigsSolver<double, SelectionRule> eigs(&op, k, m);
+    eigs.init();
+    int niter = eigs.compute();
+
+    VectorXd evals = eigs.eigenvalues();
+    MatrixXd evecs = eigs.eigenvectors();
+
+    std::cout << "computed eigenvalues D = \n" << evals.transpose() << "\n";
+    //std::cout << "computed eigenvectors U = \n" << evecs << "\n\n";
+    std::cout << "||AU - UD||_inf = " << (mat * evecs - evecs * evals.asDiagonal()).array().abs().maxCoeff() << "\n";
+    std::cout << "niter = " << niter << "\n";
+}
+
+int main()
+{
+    srand(123);
+    MatrixXd A = MatrixXd::Random(10, 10);
+
+    int k = 3;
+    int m = 6;
+
+    std::cout << "===== Largest Magnitude =====\n";
+    test<LARGEST_MAGN>(A, k, m);
+
+    std::cout << "\n===== Largest Value =====\n";
+    test<LARGEST_ALGE>(A, k, m);
+
+    std::cout << "\n===== Smallest Value =====\n";
+    test<SMALLEST_MAGN>(A, k, m);
+
+    std::cout << "\n===== Smallest Value =====\n";
+    test<SMALLEST_ALGE>(A, k, m);
+
+    std::cout << "\n===== Both Ends =====\n";
+    test<BOTH_ENDS>(A, k, m);
+
+    return 0;
+}
