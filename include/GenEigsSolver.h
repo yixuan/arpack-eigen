29525e21a7b4d12a8f2f248e27fe8fb5c5dcda2c
Yixuan Qiu <yixuan.qiu@cos.name>
2015-07-07 00:11:58 -0400

    update general eigen solver

--- include/GenEigsSolver.h
+++ include/GenEigsSolver.h
@@ -1,17 +1,23 @@
-#ifndef GENEIGSSOLVER_H
-#define GENEIGSSOLVER_H
+#ifndef GEN_EIGS_SOLVER_H
+#define GEN_EIGS_SOLVER_H
 
 #include <Eigen/Dense>
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <complex>
 #include <utility>
+#include <stdexcept>
 
-#include "MatOp.h"
 #include "SelectionRule.h"
+#include "UpperHessenbergQR.h"
+#include "MatOp/DenseGenMatProd.h"
+#include "MatOp/DenseGenShiftSolve.h"
 
 
-template <typename Scalar = double, int SelectionRule = LARGEST_MAGN>
+template < typename Scalar = double,
+           int SelectionRule = LARGEST_MAGN,
+           typename OpType = DenseGenMatProd<double> >
 class GenEigsSolver
 {
 private:
@@ -29,43 +35,51 @@ private:
     typedef Eigen::EigenSolver<Matrix> EigenSolver;
     typedef Eigen::HouseholderQR<Matrix> QRdecomp;
     typedef Eigen::HouseholderSequence<Matrix, Vector> QRQ;
+
     typedef std::pair<Complex, int> SortPair;
 
-    MatOp<Scalar> *op;       // object to conduct matrix operation,
-                             // e.g. matrix-vector product
-    int dim_n;               // dimension of matrix A
-    int nev;                 // number of eigenvalues requested
-    int nev_conj;
-    int ncv;                 // number of ritz values
+protected:
+    OpType *op;             // object to conduct matrix operation,
+                            // e.g. matrix-vector product
+private:
+    const int dim_n;        // dimension of matrix A
 
-    Matrix fac_V;            // V matrix in the Arnoldi factorization
-    Matrix fac_H;            // H matrix in the Arnoldi factorization
-    Vector fac_f;            // residual in the Arnoldi factorization
+protected:
+    const int nev;          // number of eigenvalues requested
 
-    ComplexVector ritz_val;  // ritz values
-    ComplexMatrix ritz_vec;  // ritz vectors
-    BoolArray ritz_conv;     // indicator of the convergence of ritz values
+private:
+    const int ncv;          // number of ritz values
+    int nmatop;             // number of matrix operations called
+    int niter;              // number of restarting iterations
 
-    const Scalar prec;       // precision parameter used to test convergence
-                             // prec = epsilon^(2/3)
-                             // epsilon is the machine precision,
-                             // e.g. ~= 1e-16 for the "double" type
+    Matrix fac_V;           // V matrix in the Arnoldi factorization
+    Matrix fac_H;           // H matrix in the Arnoldi factorization
+    Vector fac_f;           // residual in the Arnoldi factorization
 
-    // Matrix product in this case, and shift solve for GenEigsShiftSolver
-    virtual void matrix_operation(Scalar *x_in, Scalar *y_out)
-    {
-        op->prod(x_in, y_out);
-    }
+protected:
+    ComplexVector ritz_val; // ritz values
+
+private:
+    ComplexMatrix ritz_vec; // ritz vectors
+    BoolArray ritz_conv;    // indicator of the convergence of ritz values
+
+    const Scalar prec;      // precision parameter used to test convergence
+                            // prec = epsilon^(2/3)
+                            // epsilon is the machine precision,
+                            // e.g. ~= 1e-16 for the "double" type
 
     // Arnoldi factorization starting from step-k
-    virtual void factorize_from(int from_k, int to_m, const Vector &fk)
+    void factorize_from(int from_k, int to_m, const Vector &fk)
     {
         if(to_m <= from_k) return;
 
         fac_f = fk;
 
-        Vector v(dim_n);
+        Vector v(dim_n), w(dim_n);
         Scalar beta = 0.0;
+        // Keep the upperleft k x k submatrix of H and set other elements to 0
+        fac_H.rightCols(ncv - from_k).setZero();
+        fac_H.block(from_k, 0, ncv - from_k, from_k).setZero();
         for(int i = from_k; i <= to_m - 1; i++)
         {
             beta = fac_f.norm();
@@ -74,75 +88,146 @@ private:
             fac_H.block(i, 0, 1, i).setZero();
             fac_H(i, i - 1) = beta;
 
-            Vector w(dim_n);
-            matrix_operation(v.data(), w.data());
-            Vector h = fac_V.leftCols(i + 1).adjoint() * w;
-            fac_f = w - fac_V.leftCols(i + 1) * h;
+            op->perform_op(v.data(), w.data());
+            nmatop++;
+
+            Vector h = fac_V.leftCols(i + 1).transpose() * w;
             fac_H.block(0, i, i + 1, 1) = h;
+
+            fac_f = w - fac_V.leftCols(i + 1) * h;
+            // Correct f if it is not orthogonal to V
+            // Typically the largest absolute value occurs in
+            // the first element, i.e., <v1, f>, so we use this
+            // to test the orthogonality
+            Scalar v1f = fac_f.dot(fac_V.col(0));
+            if(v1f > prec || v1f < -prec)
+            {
+                Vector Vf(i + 1);
+                Vf.tail(i) = fac_V.block(0, 1, dim_n, i).transpose() * fac_f;
+                Vf[0] = v1f;
+                fac_f -= fac_V.leftCols(i + 1) * Vf;
+            }
         }
     }
 
+    static bool is_complex(Complex v, Scalar eps)
+    {
+        return std::abs(v.imag()) > eps;
+    }
+
+    static bool is_conj(Complex v1, Complex v2, Scalar eps)
+    {
+        return std::abs(v1 - std::conj(v2)) < eps;
+    }
+
     // Implicitly restarted Arnoldi factorization
-    virtual void restart(int k)
+    void restart(int k)
     {
         if(k >= ncv)
             return;
 
-        QRdecomp qr;
+        QRdecomp decomp_gen;
+        UpperHessenbergQR<Scalar> decomp_hb;
         Vector em(ncv);
         em.setZero();
         em[ncv - 1] = 1;
 
         for(int i = k; i < ncv; i++)
         {
-            if(is_complex(ritz_val[i]) && is_conj(ritz_val[i], ritz_val[i + 1]))
+            if(is_complex(ritz_val[i], prec) && is_conj(ritz_val[i], ritz_val[i + 1], prec))
             {
+                // H - mu * I = Q1 * R1
+                // H <- R1 * Q1 + mu * I = Q1' * H * Q1
+                // H - conj(mu) * I = Q2 * R2
+                // H <- R2 * Q2 + conj(mu) * I = Q2' * H * Q2
+                //
+                // (H - mu * I) * (H - conj(mu) * I) = Q1 * Q2 * R2 * R1 = Q * R
                 Scalar re = ritz_val[i].real();
                 Scalar s = std::norm(ritz_val[i]);
-                Matrix HH = fac_H * fac_H - 2 * re * fac_H + s * Matrix::Identity(ncv, ncv);
-                qr.compute(HH);
+                Matrix HH = fac_H;
+                HH.diagonal().array() -= 2 * re;
+                HH = fac_H * HH;
+                HH.diagonal().array() += s;
+
+                // NOTE: HH is no longer upper Hessenberg
+                decomp_gen.compute(HH);
+                QRQ Q = decomp_gen.householderQ();
+
+                // V -> VQ
+                fac_V.applyOnTheRight(Q);
+                // H -> Q'HQ
+                fac_H.applyOnTheRight(Q);
+                fac_H.applyOnTheLeft(Q.adjoint());
+                // em -> Q'em
+                em.applyOnTheLeft(Q.adjoint());
+
                 i++;
             } else {
-                qr.compute(fac_H - ritz_val[i].real() * Matrix::Identity(ncv, ncv));
+                // QR decomposition of H - mu * I, mu is real
+                fac_H.diagonal().array() -= ritz_val[i].real();
+                decomp_hb.compute(fac_H);
+
+                // V -> VQ
+                decomp_hb.apply_YQ(fac_V);
+                // H -> Q'HQ = RQ + mu * I
+                fac_H = decomp_hb.matrix_RQ();
+                fac_H.diagonal().array() += ritz_val[i].real();
+                // em -> Q'em
+                decomp_hb.apply_QtY(em);
             }
-            QRQ Q = qr.householderQ();
-            // V -> VQ
-            fac_V.applyOnTheRight(Q);
-            // H -> Q'HQ
-            fac_H.applyOnTheRight(Q);
-            fac_H.applyOnTheLeft(Q.adjoint());
-            // em -> Q'em
-            em.applyOnTheLeft(Q.adjoint());
         }
 
-        Vector fk = fac_f * em[k - 1];
+        Vector fk = fac_f * em[k - 1] + fac_V.col(k) * fac_H(k, k - 1);
         factorize_from(k, ncv, fk);
         retrieve_ritzpair();
     }
 
-    // Test convergence
-    virtual bool converged(Scalar tol)
+    // Calculate the number of converged Ritz values
+    int num_converged(Scalar tol)
     {
-        // bound = tol * max(prec, abs(theta)), theta for ritz value
-        Array bound = tol * ritz_val.head(nev).array().abs().max(prec);
-        Array resid =  ritz_vec.bottomRows(1).transpose().array().abs() * fac_f.norm();
-        ritz_conv = (resid < bound);
+        // thresh = tol * max(prec, abs(theta)), theta for ritz value
+        Array thresh = tol * ritz_val.head(nev).array().abs().max(prec);
+        Array resid = ritz_vec.template bottomRows<1>().transpose().array().abs() * fac_f.norm();
+        // Converged "wanted" ritz values
+        ritz_conv = (resid < thresh);
 
-        return ritz_conv.all();
+        return ritz_conv.cast<int>().sum();
     }
 
-    bool is_complex(Complex v)
+    // Return the adjusted nev for restarting
+    int nev_adjusted(int nconv)
     {
-        return std::abs(v.imag()) > prec;
-    }
+        int nev_new = nev;
 
-    bool is_conj(Complex v1, Complex v2)
-    {
-        return std::abs(v1 - std::conj(v2)) < prec;
+        // Increase nev by one if ritz_val[nev - 1] and
+        // ritz_val[nev] are conjugate pairs
+        if(is_complex(ritz_val[nev - 1], prec) &&
+           is_conj(ritz_val[nev - 1], ritz_val[nev], prec))
+        {
+            nev_new = nev + 1;
+        }
+        // Adjust nev_new again, according to dnaup2.f line 660~674 in ARPACK
+        nev_new = nev_new + std::min(nconv, (ncv - nev_new) / 2);
+        if(nev_new == 1 && ncv >= 6)
+            nev_new = ncv / 2;
+        else if(nev_new == 1 && ncv > 3)
+            nev_new = 2;
+
+        if(nev_new > ncv - 2)
+            nev_new = ncv - 2;
+
+        // Examine conjugate pairs again
+        if(is_complex(ritz_val[nev_new - 1], prec) &&
+           is_conj(ritz_val[nev_new - 1], ritz_val[nev_new], prec))
+        {
+            nev_new++;
+        }
+
+        return nev_new;
     }
 
     // Retrieve and sort ritz values and ritz vectors
-    virtual void retrieve_ritzpair()
+    void retrieve_ritzpair()
     {
         EigenSolver eig(fac_H);
         ComplexVector evals = eig.eigenvalues();
@@ -157,6 +242,7 @@ private:
         }
         std::sort(pairs.begin(), pairs.end(), comp);
 
+        // Copy the ritz values and vectors to ritz_val and ritz_vec, respectively
         for(int i = 0; i < ncv; i++)
         {
             ritz_val[i] = pairs[i].first;
@@ -165,17 +251,9 @@ private:
         {
             ritz_vec.col(i) = evecs.col(pairs[i].second);
         }
-
-        // If ritz_val[nev - 1] and ritz_val[nev] are conjugate pairs,
-        // let nev_conj = nev + 1
-        if(is_complex(ritz_val[nev - 1]) && is_conj(ritz_val[nev - 1], ritz_val[nev]))
-        {
-            nev_conj = nev + 1;
-        } else {
-            nev_conj = nev;
-        }
     }
 
+protected:
     // Sort the first nev Ritz pairs in decreasing magnitude order
     // This is used to return the final results
     virtual void sort_ritzpair()
@@ -204,24 +282,33 @@ private:
     }
 
 public:
-    GenEigsSolver(MatOp<Scalar> *op_, int nev_, int ncv_) :
+    GenEigsSolver(OpType *op_, int nev_, int ncv_) :
         op(op_),
         dim_n(op->rows()),
         nev(nev_),
-        nev_conj(nev_),
-        ncv(ncv_),
-        fac_V(dim_n, ncv),
-        fac_H(ncv, ncv),
-        fac_f(dim_n),
-        ritz_val(ncv),
-        ritz_vec(ncv, nev),
-        ritz_conv(nev),
+        ncv(ncv_ > dim_n ? dim_n : ncv_),
+        nmatop(0),
+        niter(0),
         prec(std::pow(std::numeric_limits<Scalar>::epsilon(), Scalar(2.0 / 3)))
-    {}
+    {
+        if(nev_ < 1 || nev_ >= dim_n)
+            throw std::invalid_argument("nev must be greater than zero and less than the size of the matrix");
+
+        if(ncv_ <= nev_)
+            throw std::invalid_argument("ncv must be greater than nev");
+    }
 
     // Initialization and clean-up
-    virtual void init(Scalar *init_coef)
+    void init(Scalar *init_resid)
     {
+        // Reset all matrices/vectors to zero
+        fac_V.resize(dim_n, ncv);
+        fac_H.resize(ncv, ncv);
+        fac_f.resize(dim_n);
+        ritz_val.resize(ncv);
+        ritz_vec.resize(ncv, nev);
+        ritz_conv.resize(nev);
+
         fac_V.setZero();
         fac_H.setZero();
         fac_f.setZero();
@@ -230,42 +317,58 @@ public:
         ritz_conv.setZero();
 
         Vector v(dim_n);
-        matrix_operation(init_coef, v.data());
-        v.normalize();
+        std::copy(init_resid, init_resid + dim_n, v.data());
+        Scalar vnorm = v.norm();
+        if(vnorm < prec)
+            throw std::invalid_argument("initial residual vector cannot be zero");
+        v /= vnorm;
+
         Vector w(dim_n);
-        matrix_operation(v.data(), w.data());
+        op->perform_op(v.data(), w.data());
+        nmatop++;
 
         fac_H(0, 0) = v.dot(w);
         fac_f = w - v * fac_H(0, 0);
         fac_V.col(0) = v;
     }
     // Initialization with random initial coefficients
-    virtual void init()
+    void init()
     {
-        Vector init_coef = Vector::Random(dim_n);
-        init(init_coef.data());
+        Vector init_resid = Vector::Random(dim_n);
+        init_resid.array() -= 0.5;
+        init(init_resid.data());
     }
 
     // Compute Ritz pairs and return the number of iteration
-    virtual int compute(int maxit = 1000, Scalar tol = 1e-10)
+    int compute(int maxit = 1000, Scalar tol = 1e-10)
     {
         // The m-step Arnoldi factorization
         factorize_from(1, ncv, fac_f);
         retrieve_ritzpair();
         // Restarting
-        int i = 0;
+        int i, nconv, nev_adj;
         for(i = 0; i < maxit; i++)
         {
-            if(converged(tol))
+            nconv = num_converged(tol);
+            if(nconv >= nev)
                 break;
 
-            restart(nev_conj);
+            nev_adj = nev_adjusted(nconv);
+            restart(nev_adj);
         }
         // Sorting results
         sort_ritzpair();
 
-        return i + 1;
+        niter += i + 1;
+
+        return std::min(nev, nconv);
     }
+
+    // Return the number of restarting iterations
+    int num_iterations() { return niter; }
+
+    // Return the number of matrix operations
+    int num_operations() { return nmatop; }
 
     // Return converged eigenvalues
     virtual ComplexVector eigenvalues()
@@ -316,4 +419,34 @@ public:
 };
 
 
-#endif // GENEIGSSOLVER_H
+
+
+
+template <typename Scalar = double,
+          int SelectionRule = LARGEST_MAGN,
+          typename OpType = DenseGenShiftSolve<double> >
+class GenEigsRealShiftSolver: public GenEigsSolver<Scalar, SelectionRule, OpType>
+{
+private:
+    typedef std::complex<Scalar> Complex;
+    typedef Eigen::Array<Complex, Eigen::Dynamic, 1> ComplexArray;
+
+    Scalar sigma;
+
+    // First transform back the ritz values, and then sort
+    void sort_ritzpair()
+    {
+        ComplexArray ritz_val_org = Scalar(1.0) / this->ritz_val.head(this->nev).array() + sigma;
+        this->ritz_val.head(this->nev) = ritz_val_org;
+        GenEigsSolver<Scalar, SelectionRule, OpType>::sort_ritzpair();
+    }
+public:
+    GenEigsRealShiftSolver(OpType *op_, int nev_, int ncv_, Scalar sigma_) :
+        GenEigsSolver<Scalar, SelectionRule, OpType>(op_, nev_, ncv_),
+        sigma(sigma_)
+    {
+        this->op->set_shift(sigma);
+    }
+};
+
+#endif // GEN_EIGS_SOLVER_H
---
+++ test/GenEigsRealShift.cpp
@@ -0,0 +1,72 @@
+#include <Eigen/Dense>
+#include <iostream>
+
+#include <GenEigsSolver.h>
+#include <MatOp/DenseGenShiftSolve.h>
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+typedef Eigen::MatrixXd Matrix;
+typedef Eigen::VectorXd Vector;
+typedef Eigen::MatrixXcd ComplexMatrix;
+typedef Eigen::VectorXcd ComplexVector;
+
+template <int SelectionRule>
+void run_test(Matrix &mat, int k, int m, double sigma)
+{
+    DenseGenShiftSolve<double> op(mat);
+    GenEigsRealShiftSolver<double, SelectionRule, DenseGenShiftSolve<double>> eigs(&op, k, m, sigma);
+    eigs.init();
+    int nconv = eigs.compute();
+    int niter = eigs.num_iterations();
+    int nops = eigs.num_operations();
+
+    REQUIRE( nconv > 0 );
+
+    ComplexVector evals = eigs.eigenvalues();
+    ComplexMatrix evecs = eigs.eigenvectors();
+
+    ComplexMatrix err = mat * evecs - evecs * evals.asDiagonal();
+
+    INFO( "nconv = " << nconv );
+    INFO( "niter = " << niter );
+    INFO( "nops = " << nops );
+    INFO( "||AU - UD||_inf = " << err.array().abs().maxCoeff() );
+    REQUIRE( err.array().abs().maxCoeff() == Approx(0.0) );
+}
+
+TEST_CASE("Eigensolver of general real matrix", "[eigs_gen]")
+{
+    srand(123);
+    Matrix A = Eigen::MatrixXd::Random(10, 10);
+
+    int k = 3;
+    int m = 6;
+    double sigma = 0.0;
+
+    SECTION( "Largest Magnitude" )
+    {
+        run_test<LARGEST_MAGN>(A, k, m, sigma);
+    }
+    SECTION( "Largest Real Part" )
+    {
+        run_test<LARGEST_REAL>(A, k, m, sigma);
+    }
+    SECTION( "Largest Imaginary Part" )
+    {
+        run_test<LARGEST_IMAG>(A, k, m, sigma);
+    }
+    SECTION( "Smallest Magnitude" )
+    {
+        run_test<SMALLEST_MAGN>(A, k, m, sigma);
+    }
+    SECTION( "Smallest Real Part" )
+    {
+        run_test<SMALLEST_REAL>(A, k, m, sigma);
+    }
+    SECTION( "Smallest Imaginary Part" )
+    {
+        run_test<SMALLEST_IMAG>(A, k, m, sigma);
+    }
+}
