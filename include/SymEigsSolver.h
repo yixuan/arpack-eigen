0f950dc302a1dbc5cea5958ce7b165768cdf9410
Yixuan Qiu <yixuan.qiu@cos.name>
2015-08-22 19:18:35 -0400

    license information

--- include/SymEigsSolver.h
+++ include/SymEigsSolver.h
@@ -1,3 +1,9 @@
+// Copyright (C) 2015 Yixuan Qiu <yixuan.qiu@cos.name>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #ifndef SYM_EIGS_SOLVER_H
 #define SYM_EIGS_SOLVER_H
 
